34163d1f9dd90bbeb96f92a09c50b708b7193803
xXCleanXx <erdwegsebastian@hotmail.com>
2022-09-17 01:47:42 +0200

    + AnnotationsExtensions + CleanLib.Wpf/Extensions/EnumExtension

---
+++ CleanLib.Wpf/Extensions/EnumExtension.cs
@@ -0,0 +1,37 @@
+﻿using CleanLib.Common.Annotations.Extensions;
+using System;
+using System.Linq;
+using System.Windows.Markup;
+
+namespace CleanLib.Wpf.Extensions;
+
+public class EnumExtension : MarkupExtension {
+    private Type _enumType;
+
+    public Type EnumType {
+        get => this._enumType;
+        set {
+            if (value is null) throw new ArgumentNullException(nameof(this.EnumType), "Enum cannot be null!");
+            if (!value.IsEnum) throw new ArgumentException("Type is not an enum!", nameof(this.EnumType));
+
+            this._enumType = value;
+        }
+    }
+
+    public EnumExtension(Type enumType) {
+        this.EnumType = enumType;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+        => from Enum value in Enum.GetValues(this.EnumType) select value.GetEnumDescription();
+
+    public class EnumValue {
+        public Enum Value { get; }
+        public string Description { get; }
+
+        public EnumValue(Enum value, string description) {
+            this.Value = value;
+            this.Description = description;
+        }
+    }
+}
